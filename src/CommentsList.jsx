ba2f431b87d6718c653f0c1cfde5833b7510e7d5
Badi1298 <serbandavid83@yahoo.com>
2024-06-09 21:18:03 +0300

    comment moderation

--- src/CommentsList.jsx
+++ src/CommentsList.jsx
@@ -2,7 +2,9 @@ export default function PostList({ comments }) {
 	return (
 		<ul>
 			{comments.map((comment) => (
-				<li key={comment.id}>{comment.content}</li>
+				<li key={comment.id}>
+					{comment.status === 'pending' ? 'Awaiting moderation' : comment.status === 'approved' ? comment.content : 'Comment rejected'}
+				</li>
 			))}
 		</ul>
 	);
